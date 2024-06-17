b010ad72c9a40923d32eceb2ffb52a75c2520838
Yuhang <472617147@qq.com>
2024-06-17 17:30:38 +0800

    fix: support VisionOS

--- Sources/Yams/Representer.swift
+++ Sources/Yams/Representer.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-#if os(iOS) || os(macOS) || os(watchOS) || os(tvOS)
+#if os(iOS) || os(macOS) || os(watchOS) || os(tvOS) || os(VisionOS)
 import Darwin
 private let cpow: (_: Double, _: Double) -> Double = Darwin.pow
 #elseif os(Windows)
